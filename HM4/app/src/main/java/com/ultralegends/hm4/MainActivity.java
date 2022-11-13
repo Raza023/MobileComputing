6ff52655b93697744fabc70edc9f770181486ab9
Hassan <hr770735@gmail.com>
2022-11-13 23:40:06 +0500

    HM4-quiz assignment 5 questions game finalized

--- HM4/app/src/main/java/com/ultralegends/hm4/MainActivity.java
+++ HM4/app/src/main/java/com/ultralegends/hm4/MainActivity.java
@@ -11,18 +11,21 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView q1, q2, showResult;
-    RadioGroup q1Group, q2Group;
-    RadioButton sample,q1o1,q1o2,q1o3,q1o4,q2o1,q2o2,q2o3,q2o4;
+    TextView q1, q2, q3, q4, q5, showResult;
+    RadioGroup q1Group, q2Group, q3Group, q4Group, q5Group;
+    RadioButton sample,q1o1,q1o2,q1o3,q1o4,q2o1,q2o2,q2o3,q2o4,q3o1,q3o2,q3o3,q3o4,q4o1,q4o2,q4o3,q4o4,q5o1,q5o2,q5o3,q5o4;
     Button resultbtn;
     int counter=0;
 
-    String [] questions = {"Who is the king of Jungle?","What is the SI unit of distance?"};
+    String [] questions = {"Who is the king of Jungle?","What is the SI unit of distance?","What is the capital of Pakistan?","Which instrument is used to see micro-organisms?","Who is the founder of Pakistan?"};
 
     int noOfQuestions = questions.length;
 
-    String [] options1 = {"Elephant","Lion","Tiger","Deer"};
+    String [] options1 = {"Elephant","Tiger","Deer","Lion"};
     String [] options2 = {"meter","Kelvin","liter","Joule"};
+    String [] options3 = {"Lahore","Karachi","Islamabad","Peshawar"};
+    String [] options4 = {"Telescope","Microscope","Stathoscope","Miniscope"};
+    String [] options5 = {"Muhammad Ali Jinnah","Allama Iqbal","Sir Syed Ahmed","Liaqat Ali Khan"};
 
     int noOfOptions = 4;
 
@@ -45,24 +48,59 @@ public class MainActivity extends AppCompatActivity {
         q2o3 = findViewById(R.id.q2o3);
         q2o4 = findViewById(R.id.q2o4);
 
+        q3 = findViewById(R.id.q3);
+        q3Group = findViewById(R.id.q3group);
+        q3o1 = findViewById(R.id.q3o1);
+        q3o2 = findViewById(R.id.q3o2);
+        q3o3 = findViewById(R.id.q3o3);
+        q3o4 = findViewById(R.id.q3o4);
+
+        q4 = findViewById(R.id.q4);
+        q4Group = findViewById(R.id.q4group);
+        q4o1 = findViewById(R.id.q4o1);
+        q4o2 = findViewById(R.id.q4o2);
+        q4o3 = findViewById(R.id.q4o3);
+        q4o4 = findViewById(R.id.q4o4);
+
+        q5 = findViewById(R.id.q5);
+        q5Group = findViewById(R.id.q5group);
+        q5o1 = findViewById(R.id.q5o1);
+        q5o2 = findViewById(R.id.q5o2);
+        q5o3 = findViewById(R.id.q5o3);
+        q5o4 = findViewById(R.id.q5o4);
+
+
         showResult = findViewById(R.id.showResult);
         resultbtn = findViewById(R.id.resultbtn);
 
         shuffleArray(questions);
         shuffleArray(options1);
         shuffleArray(options2);
+        shuffleArray(options3);
+        shuffleArray(options4);
+        shuffleArray(options5);
 
         q1.setText(questions[0]);
         setOptionsOfThisQustion(q1,q1o1,q1o2,q1o3,q1o4);
         q2.setText(questions[1]);
         setOptionsOfThisQustion(q2,q2o1,q2o2,q2o3,q2o4);
+        q3.setText(questions[2]);
+        setOptionsOfThisQustion(q3,q3o1,q3o2,q3o3,q3o4);
+        q4.setText(questions[3]);
+        setOptionsOfThisQustion(q4,q4o1,q4o2,q4o3,q4o4);
+        q5.setText(questions[4]);
+        setOptionsOfThisQustion(q5,q5o1,q5o2,q5o3,q5o4);
 
         resultbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 CheckAnswer(q1, q1Group);
                 CheckAnswer(q2, q2Group);
-                if(counter!=0)
+                CheckAnswer(q3, q3Group);
+                CheckAnswer(q4, q4Group);
+                CheckAnswer(q5, q5Group);
+
+                if(counter<=5)
                 {
                     showResult.setText("Your result: "+counter+"/2");
                 }
@@ -86,6 +124,27 @@ public class MainActivity extends AppCompatActivity {
             o3.setText(options2[2]);
             o4.setText(options2[3]);
         }
+        else if(question.getText().toString().equals("What is the capital of Pakistan?"))
+        {
+            o1.setText(options3[0]);
+            o2.setText(options3[1]);
+            o3.setText(options3[2]);
+            o4.setText(options3[3]);
+        }
+        else if(question.getText().toString().equals("Which instrument is used to see micro-organisms?"))
+        {
+            o1.setText(options4[0]);
+            o2.setText(options4[1]);
+            o3.setText(options4[2]);
+            o4.setText(options4[3]);
+        }
+        else if(question.getText().toString().equals("Who is the founder of Pakistan?"))
+        {
+            o1.setText(options5[0]);
+            o2.setText(options5[1]);
+            o3.setText(options5[2]);
+            o4.setText(options5[3]);
+        }
     }
 
     public void CheckAnswer(TextView question, RadioGroup qGroup)
@@ -102,6 +161,27 @@ public class MainActivity extends AppCompatActivity {
         else if(question.getText().toString().equals("What is the SI unit of distance?"))
         {
             if(sample.getText().toString().equals("meter"))
+            {
+                counter++;
+            }
+        }
+        else if(question.getText().toString().equals("What is the capital of Pakistan?"))
+        {
+            if(sample.getText().toString().equals("Islamabad"))
+            {
+                counter++;
+            }
+        }
+        else if(question.getText().toString().equals("Which instrument is used to see micro-organisms?"))
+        {
+            if(sample.getText().toString().equals("Microscope"))
+            {
+                counter++;
+            }
+        }
+        else if(question.getText().toString().equals("Who is the founder of Pakistan?"))
+        {
+            if(sample.getText().toString().equals("Muhammad Ali Jinnah"))
             {
                 counter++;
             }
