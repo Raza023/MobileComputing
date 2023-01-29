999c1c94a153e286e2dd0fb067290827c6b96f3b
Hassan <hr770735@gmail.com>
2023-01-30 00:39:19 +0500

    HM10 - DB_CRUD
    Student class

---
+++ HM10crudDB/app/src/main/java/com/ultralegends/hm10cruddb/Student.java
@@ -0,0 +1,53 @@
+package com.ultralegends.hm10cruddb;
+
+public class Student {
+//    int id;
+    private String name;
+    private String rollNo;
+    private boolean isEnroll;
+
+    public Student() {
+        this.name = "";
+        this.rollNo = "";
+        this.isEnroll = false;
+    }
+
+    public Student(String name, String rollNo, boolean isEnroll) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.isEnroll = isEnroll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(String rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public boolean isEnroll() {
+        return isEnroll;
+    }
+
+    public void setEnroll(boolean enroll) {
+        isEnroll = enroll;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", rollNo='" + rollNo + '\'' +
+                ", isEnroll=" + isEnroll +
+                '}';
+    }
+}
