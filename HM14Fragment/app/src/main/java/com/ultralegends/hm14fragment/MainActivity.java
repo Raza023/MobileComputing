f815e2b31ee3fe25a68dd78146cf3fa969559b5c
Hassan <hr770735@gmail.com>
2023-02-13 09:21:43 +0500

    HM14 - Fragment
    Fragment Two added

--- HM14Fragment/app/src/main/java/com/ultralegends/hm14fragment/MainActivity.java
+++ HM14Fragment/app/src/main/java/com/ultralegends/hm14fragment/MainActivity.java
@@ -1,14 +1,40 @@
 package com.ultralegends.hm14fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnOne,btnTwo;
+    FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnOne = findViewById(R.id.btnOne);
+        btnTwo = findViewById(R.id.btnTwo);
+
+        btnOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentOne one = new FragmentOne();
+                transaction = getSupportFragmentManager().beginTransaction();
+//                transaction.replace()
+            }
+        });
+
+        btnTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        
     }
 }
